baa06cdeb21174ad54064d39651bb269f13974a4
ArunprakashLfs <arunprakash7871@gmail.com>
2023-03-25 13:40:48 +0530

    input getted from javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict'
+//Getting input from user
+const nameEl = document.getElementById("input-Name");
+const taskEl = document.getElementById("input-task");
+const timeEl = document.getElementById("input-time");
+const formEl = document.getElementById("form");
+
+//displaying output 
+const outputEl = document.getElementById("task-items");
+
+
